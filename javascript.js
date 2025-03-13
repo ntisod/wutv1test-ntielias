209c92139803b51f9669d4de48ad320ceff6afdb
ntielias <elias.rmaili@elev.ga.ntig.se>
2025-03-13 11:20:17 +0100

    javascript

---
+++ javascript.js
@@ -0,0 +1,27 @@
+/* kommentar med
+flera rader */
+
+//kommentar med en rad
+
+
+function visaDialog(){
+    alert('Hej Världen!');
+}
+
+//2025-03-13
+function authorDialog(){
+    alert("Jag har gjort webbsidan 2025")
+}
+
+function colorDialog(){
+    document.getElementById("rubrik").innerHTML = "Elias";
+    document.getElementById("text").style.backgroundColor = "lightblue";
+}
+
+function imageChanger(){
+    document.getElementById("image").src = "spaghetti.jpg";
+}
+
+function imageChanger2(){
+    document.getElementById("image").src = "timberners.png";
+}
